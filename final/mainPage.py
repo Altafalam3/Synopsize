740514f82c0620b5b4795bb01a399d4f78417923
Altafalam3 <altaf.alam0032@gmail.com>
2023-04-04 20:15:46 +0530

    SUA DONE

--- final/mainPage.py
+++ final/mainPage.py
@@ -54,13 +54,7 @@ class AudioToTextGUI:
 
             text = r.recognize_google(audio)
 
-            #Dummy text to test functionality
-
-            # text = """Machine learning (ML) is the scientific study of algorithms and statistical models that computer systems use to progressively improve their performance on a specific task. Machine learning algorithms build a mathematical model of sample data, known as "training data", in order to make predictions or decisions without being explicitly programmed to perform the task. Machine learning algorithms are used in the applications of email filtering, detection of network intruders, and computer vision, where it is infeasible to develop an algorithm of specific instructions for performing the task. Machine learning is closely related to computational statistics, which focuses on making predictions using computers. The study of mathematical optimization delivers methods, theory and application domains to the field of machine learning. Data mining is a field of study within machine learning, and focuses on exploratory data analysis through unsupervised learning. In its application across business problems, machine learning is also referred to as predictive analytics.
-            # Machine learning (ML) is the scientific study of algorithms and statistical models that computer systems use to progressively improve their performance on a specific task. Machine learning algorithms build a mathematical model of sample data, known as "training data", in order to make predictions or decisions without being explicitly programmed to perform the task. Machine learning algorithms are used in the applications of email filtering, detection of network intruders, and computer vision, where it is infeasible to develop an algorithm of specific instructions for performing the task. Machine learning is closely related to computational statistics, which focuses on making predictions using computers. The study of mathematical optimization delivers methods, theory and application domains to the field of machine learning. Data mining is a field of study within machine learning, and focuses on exploratory data analysis through unsupervised learning. In its application across business problems, machine learning is also referred to as predictive analytics."""
-
             print("Converted audio is: " + text)
-
 
             # define the summary length as a percentage of the input message
             SUMMARY_PERCENTAGE = 0.25
@@ -71,7 +65,6 @@ class AudioToTextGUI:
             # Use set() to eliminate duplicates
             stop_word  = list(STOP_WORDS)
             punctuations = list(punctuation)
-
 
             stopwords = set(stop_word+punctuations)
             
